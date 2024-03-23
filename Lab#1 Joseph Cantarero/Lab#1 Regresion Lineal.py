f6267706f41198be02cf3fea8794993ae69961fb
Jope007 <JOSEPH.CANTARERO@ucr.ac.cr>
2024-03-23 10:54:43 -0600

    Correcciones Lab#1

--- Lab#1 Joseph Cantarero/Lab#1 Regresion Lineal.py
+++ Lab#1 Joseph Cantarero/Lab#1 Regresion Lineal.py
@@ -4,30 +4,30 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 import matplotlib.pyplot as plt
 
-# Paso 2: Generar datos de ejemplo
+# Step 1: Generate example data
 np.random.seed(0)
-tamano_vivienda = 2 * np.random.rand(100, 1)
-precio_vivienda = 3 + 4 * tamano_vivienda + np.random.randn(100, 1)
+house_size = 2 * np.random.rand(100, 1)
+house_price = 3 + 4 * house_size + np.random.randn(100, 1)
 
-# Paso 3: Dividir los datos en conjuntos de entrenamiento y prueba
-X_train, X_test, y_train, y_test = train_test_split(tamano_vivienda, precio_vivienda, test_size=0.2, random_state=42)
+# Step 2: Divide the data in training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(house_size, house_price, test_size=0.2, random_state=42)
 
-# Paso 4: Crear y entrenar el modelo de regresión lineal
+# Step 3: Create and train the lineal regression model
 model = LinearRegression()
 model.fit(X_train, y_train)
 
-# Paso 5: Hacer predicciones
+# Step 4: Predict results with the model
 y_pred = model.predict(X_test)
 
-# Paso 6: Calcular el error cuadrático medio
+# Step 5: Calculate the mean squared error
 mse = mean_squared_error(y_test, y_pred)
 print("Error Cuadrático Medio:", mse)
 
-# Paso 7: Calcular el coeficiente de determinación (R-cuadrado)
+# Step 6: Calculate the r2 score
 r2 = r2_score(y_test, y_pred)
 print("Coeficiente de Determinación (R-cuadrado):", r2)
 
-# Paso 8: Visualizar los resultados
+# Step 7: Visualize the results
 plt.scatter(X_test, y_test, color='blue')
 plt.plot(X_test, y_pred, color='red')
 plt.title('Regresión Lineal Simple')
